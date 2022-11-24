3bd2fc88b116c325b7db544fab4bcc992caf1e92
Jon Christian Bista <jnchrstnbst@gmail.com>
2022-11-25 00:55:16 +0800

    updates
    upddates

---
+++ main.js
@@ -0,0 +1,27 @@
+const card = document.querySelector(".card__inner");
+
+card.addEventListener("click", function (e) {
+  card.classList.toggle('is-flipped');
+});
+
+const card2 = document.querySelector(".card__inner2");
+
+card2.addEventListener("click", function (e) {
+  card2.classList.toggle('is-flipped');
+});
+
+const card3 = document.querySelector(".card__inner3");
+
+card3.addEventListener("click", function (e) {
+  card3.classList.toggle('is-flipped');
+});
+
+
+
+
+$(document).ready(function(){
+  $('#icon').click(function(){
+   $('ul').toggleClass('show');
+  });
+});
+
