920879bfbac13b6431abfac4ec4483eca87693bc
Jon Christian Bista <jnchrstnbst@gmail.com>
2022-12-11 00:48:19 +0800

    Phone Validation
    I use regex ((^(\+)(\d){12}$)|(^\d{11}$))

--- main.js
+++ main.js
@@ -4,5 +4,18 @@ $(document).ready(function(){
   });
 });
 
-
+function sendEmail(){
+  Email.send({
+    SecureToken:"27757254-f7ae-45e6-8b70-baad8f77e355",
+    To : 'jnchrstnbst@gmail.com',
+    From : "jonbista21.parsu@gmail.com",
+    Subject : "New Contact Form Enquiry",
+    Body : "Name: " + document.getElementById("name").value
+      + "<br> Email: " + document.getElementById("email").value
+      + "<br> Phone Number: " + document.getElementById("phone").value
+      + "<br> Message: " + document.getElementById("message").value
+  }).then(
+  message => alert("Message Sent Successfully")
+  );
+}
 
